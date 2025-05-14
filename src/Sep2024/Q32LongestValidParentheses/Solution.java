d50e604533bd907e4e153d7a0b2539a311aac770
caTrixLiu <lyr20070621@gmail.com>
2025-05-14 18:55:15 +0800

    added question for q32

---
+++ src/Sep2024/Q32LongestValidParentheses/Solution.java
@@ -0,0 +1,28 @@
+package Sep2024.Q32LongestValidParentheses;
+// https://leetcode.com/problems/longest-valid-parentheses
+
+/**
+ * <h1>Longest Valid Parentheses</h1>
+ *
+ * Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+ *
+ * <h3>Example 1:</h3>
+ * <code>Input: s = "(()"</code>
+ * <code>Output: 2</code>
+ * <p>Explanation: The longest valid parentheses substring is "()".</p>
+ *
+ * <h3>Example 2:</h3>
+ * <code>Input: s = ")()())"</code>
+ * <code>Output: 4</code>
+ * <p>Explanation: The longest valid parentheses substring is "()()".</p>
+ *
+ * <h3>Example3: </h3>
+ * <code>Input: s = ""</code>
+ * <code>Output: 0</code>
+ * */
+public class Solution {
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
+}
+
